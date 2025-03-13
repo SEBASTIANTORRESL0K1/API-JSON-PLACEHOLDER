742f2441c3efc139a70406a27e2c5884a12afe1a
SEBASTIANTORRESL0K1 <svelasco13@ucol.mx>
2025-03-13 09:00:52 -0600

    Se agrego una función POST y una función DELETE

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let contador=100;
 const btnMostrarPosts=document.getElementById('btnMostrarPosts');
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -26,6 +27,8 @@ btnMostrarPosts.addEventListener('click',()=>{
                 <div class="divBotones">
                     <button onclick="verComentarios(${post.id})" class="btnPost" id="btnVerComentarios${post.id}"> Ver comentarios</button>
                     <button onclick="ocultarComentarios(${post.id})" class="visibilidad btnPost" id="btnOcultarComentarios${post.id}"> Ocultar comentarios</button>
+                    <button onclick="eliminarPost(${post.id})" class="btnPost"> - </button>
+
                 </div>
                 <div id="divComentarios${post.id}"></div>
             </div>
@@ -34,6 +37,17 @@ btnMostrarPosts.addEventListener('click',()=>{
     })
     const btnOcultarPosts=document.getElementById('btnOcultarPosts');
     btnOcultarPosts.classList.remove('visibilidad');
+    const formulario=document.getElementById('formulario');
+    formulario.classList.remove('visibilidad');
+    formulario.innerHTML=`
+        <form id="form">
+            <label>Título del Post</label>
+            <input type="text" id="postText">
+            <label>Cuerpo del Post</label>
+            <textarea type="text" id="postBody"></textarea>
+            <button onclick="enviarPost()" type="button">Enviar Post</button>
+        </form>
+    `;
 })
 
 function verComentarios(idPost){
@@ -77,4 +91,60 @@ const selectUsuarios=document.getElementById("selectUsuarios");
         btnMostrarPosts.classList.remove("visibilidad");
         btnOcultarPosts.classList.add("visibilidad");
         document.getElementById("divPosts").innerHTML="";
+        const divForm=document.getElementById("formulario");
+        divForm.classList.add("visibilidad");
     })
+
+function enviarPost(){
+    const selectUsuarios=document.getElementById('selectUsuarios');
+    let id=selectUsuarios.value;
+    let postText=document.getElementById("postText").value;
+    let postBody=document.getElementById("postBody").value;
+    let post={"userId":id,"id":++contador,title:postText,body:postBody};
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: postText,
+          body: postBody,
+          userId: id,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    document.getElementById("postText").value="";
+    document.getElementById("postBody").value="";
+    const divPosts=document.getElementById('divPosts');
+    divPosts.innerHTML+=`
+    <div id="divPost${post.id}" class="divPost">
+        <h3 id="titlePost${post.id}" class="h3Post">${post.title}</h3>
+        <p id="bodyPost${post.id}" class="pPost">${post.body}</p>
+        <div class="divBotones">
+            <button onclick="verComentarios(${post.id})" class="btnPost" id="btnVerComentarios${post.id}"> Ver comentarios</button>
+            <button onclick="ocultarComentarios(${post.id})" class="visibilidad btnPost" id="btnOcultarComentarios${post.id}"> Ocultar comentarios</button>
+            <button onclick="eliminarPost(${post.id})" class="btnPost"> - </button>
+
+        </div>
+        <div id="divComentarios${post.id}"></div>
+    </div>
+    ` ;
+}
+function eliminarPost(id){
+    let divPosts=document.getElementById("divPosts");
+    let postEliminar=document.getElementById(`divPost${id}`);
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'DELETE',
+    })
+      .then((response) => {
+        if(response.ok){
+            console.log("Eliminado");
+            divPosts.removeChild(postEliminar);
+
+        }
+        else{
+            console.log("No se eliminó");
+        }
+      })
+}
